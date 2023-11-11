4ee114ea677f9378aad2a8e507568b480656c22c
Jakub Narębski <jnareb@mat.umk.pl>
2023-11-12 00:24:05 +0100

    Add setup.py created by cookiecutter-data-science
    Having basic setup.py makes project pip installable (`pip install -e .`),
    so 'src' can be imported.  More modern solution would be to use
    pyproject.toml (perhaps with setup.py as fallback).
    
    Created with the following series of command, then adjusted:
    
    $ pip install cookiecutter
    $ cookiecutter https://github.com/drivendata/cookiecutter-data-science

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description="Code for MSR'24 Mining Challenge: https://2024.msrconf.org/track/msr-2024-mining-challenge",
+    author='NCU-SI',
+    license='MIT',
+)
